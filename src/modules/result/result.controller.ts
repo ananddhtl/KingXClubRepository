e23cdeb3c2e8cf082bc9c95a9ccbe7901bebd177
Rahul Shah <075bct063.rahul@pcampus.edu.np>
2024-07-29 23:07:40 +0545

    remove time check from result publish

--- src/modules/result/result.controller.ts
+++ src/modules/result/result.controller.ts
@@ -3,28 +3,28 @@ import { HttpException, HttpStatus } from '@nestjs/common';
 import ResultService from './result.service';
 import { PublishResultDto } from './dtos/publish-result.dto';
 import httpStatus from 'http-status';
-const CLUBS = [
-  {
-    place: 'Club Panther',
-    time: ['00:07:00', '00:13:00', '00:17:00', '01:01:00'],
-  },
-  {
-    place: 'Club Tiger',
-    time: ['00:08:00', '00:14:00', '00:18:00', '00:22:00'],
-  },
-  {
-    place: 'Club Lion',
-    time: ['00:09:00', '00:13:00', '00:19:00', '00:23:00'],
-  },
-  {
-    place: 'Club Puma',
-    time: ['00:10:00', '00:16:00', '00:20:00', '01:00:00'],
-  },
-  {
-    place: 'Club Cobra',
-    time: ['00:11:00', '00:17:00', '00:21:00', '01:02:00'],
-  },
-];
+// const CLUBS = [
+//   {
+//     place: 'Club Panther',
+//     time: ['00:07:00', '00:13:00', '00:17:00', '01:01:00'],
+//   },
+//   {
+//     place: 'Club Tiger',
+//     time: ['00:08:00', '00:14:00', '00:18:00', '00:22:00'],
+//   },
+//   {
+//     place: 'Club Lion',
+//     time: ['00:09:00', '00:13:00', '00:19:00', '00:23:00'],
+//   },
+//   {
+//     place: 'Club Puma',
+//     time: ['00:10:00', '00:16:00', '00:20:00', '01:00:00'],
+//   },
+//   {
+//     place: 'Club Cobra',
+//     time: ['00:11:00', '00:17:00', '00:21:00', '01:02:00'],
+//   },
+// ];
 
 export class ResultController {
   static instance: null | ResultController;
@@ -44,24 +44,23 @@ export class ResultController {
       const { time, place, ticketNumber, position }: PublishResultDto = req.body;
       if (ticketNumber.length !== 3) throw new HttpException('Number must be 3 digit', httpStatus.CONFLICT);
 
-      const possibleTime = (CLUBS.find(club => club.place === place)?.time || [])
-        .map(timestamp => {
-          const time = new Date().setHours(
-            Number(timestamp.split(':')[0]) * 24 + Number(timestamp.split(':')[1]),
-            Number(timestamp.split(':')[2]),
-            0,
-            0,
-          );
-          if (Number(timestamp.split(':')[0]) === 1) {
-            const previousTime = new Date().setHours(Number(timestamp.split(':')[1]), Number(timestamp.split(':')[2]), 0, 0);
-            return [time, previousTime];
-          }
-          return time;
-        })
-        .flat();
-      console.log('time', possibleTime, time);
-
-      if (!possibleTime.includes(time)) throw new HttpException('Result Time doesnot match, please try again', httpStatus.CONFLICT);
+      // const possibleTime = (CLUBS.find(club => club.place === place)?.time || [])
+      //   .map(timestamp => {
+      //     const time = new Date().setHours(
+      //       Number(timestamp.split(':')[0]) * 24 + Number(timestamp.split(':')[1]),
+      //       Number(timestamp.split(':')[2]),
+      //       0,
+      //       0,
+      //     );
+      //     if (Number(timestamp.split(':')[0]) === 1) {
+      //       const previousTime = new Date().setHours(Number(timestamp.split(':')[1]), Number(timestamp.split(':')[2]), 0, 0);
+      //       return [time, previousTime];
+      //     }
+      //     return time;
+      //   })
+      //   .flat();
+      //   //time is getting wrong, possibily due to different time zone
+      // if (!possibleTime.includes(time)) throw new HttpException('Result Time doesnot match, please try again', httpStatus.CONFLICT);
 
       const response =
         position === 'Open'
